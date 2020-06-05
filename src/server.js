a592466878a9290e1cac66d9305d4d900da64cbf
raphamatsu <raphael_matsumoto@estudante.sc.senai.br>
2020-06-05 19:03:03 -0300

    Aula 4, refatoração + nunjuncks

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const server = express()
+
+// confirgurar pasta pública
+server.use(express.static("public"))
+
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//configurar caminhos da minha aplicação
+//página inicial
+// req: requisição
+//res: resposta
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Seu marketplace de coleta de resíduos"})
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+})
+
+//ligar o servidor
+server.listen(3000)
